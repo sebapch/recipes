fc348e4dcab621d479e8629cc27d83776c7aaba1
Sebastian Medina <39243497+sebapch@users.noreply.github.com>
2023-01-18 21:42:34 -0300

    fix title 0.1.3

--- recipes/app/layout.js
+++ recipes/app/layout.js
@@ -6,7 +6,7 @@ export default function RootLayout({ children }) {
   return (
     <html>
       <head>
-        <title>My app with next 13</title>
+        <title>Que comemos hoy?</title>
       </head>
       <body>
          {/*  <Navigation /> */}
